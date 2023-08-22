a296d38f5453c87c362114d9cd262c873c2f8615
Jiehong Jiang <jjiangweilan@gmail.com>
2023-08-23 00:01:51 +0800

    add deprecated warning to GetBuffer

--- Source/Engine/GfxDriver/ShaderResource.hpp
+++ Source/Engine/GfxDriver/ShaderResource.hpp
@@ -24,12 +24,17 @@ public:
     using BufferMemberInfoMap = std::unordered_map<std::string, BufferMemberInfo>;
 
     virtual ~ShaderResource(){};
+    [[deprecated("")]]
     virtual RefPtr<Buffer> GetBuffer(const std::string& obj, BufferMemberInfoMap& memberInfo) = 0;
     virtual void SetTexture(const std::string& name, RefPtr<Image> texture) = 0;
-    // virtual void SetStorage(std::string_view name, RefPtr<StorageBuffer> storageBuffer) = 0;
+    // virtual void SetStorage(std::string_view name, RefPtr<StorageBuffer>
+    // storageBuffer) = 0;
     virtual bool HasPushConstnat(const std::string& obj) = 0;
     virtual RefPtr<ShaderProgram> GetShader() = 0;
-    virtual ShaderResourceFrequency GetFrequency() { return frequency; }
+    virtual ShaderResourceFrequency GetFrequency()
+    {
+        return frequency;
+    }
 
 protected:
     ShaderResourceFrequency frequency;
