2dea1dce70b5249505af21292c7d0c9483c8e096
Jiehong Jiang <jjiangweilan@gmail.com>
2024-01-01 23:53:18 +0800

    refactor: register component to inspector registery

---
+++ Source/Editor/Inspectors/Components/CameraInspector.cpp
@@ -0,0 +1,61 @@
+#include "../../EditorState.hpp"
+#include "../Inspector.hpp"
+#include "Core/Component/Camera.hpp"
+#include "Core/Scene/Scene.hpp"
+#include "Rendering/FrameGraph/FrameGraph.hpp"
+
+namespace Editor
+{
+class CameraInspector : public Inspector<Camera>
+{
+public:
+    void DrawInspector(GameEditor& editor) override
+    {
+        if (ImGui::Button("Set as main camera"))
+        {
+            if (EditorState::activeScene)
+            {
+                EditorState::activeScene->SetMainCamera(target);
+            }
+        }
+
+        auto frameGraph = target->GetFrameGraph();
+        const char* buttonName = "empty";
+        if (frameGraph)
+        {
+            buttonName = frameGraph->GetName().c_str();
+        }
+        ImGui::Text("Frame Graph: ");
+        ImGui::SameLine();
+        if (ImGui::Button(buttonName))
+        {
+            EditorState::selectedObject = frameGraph;
+        }
+
+        if (ImGui::BeginDragDropTarget())
+        {
+            auto payload = ImGui::AcceptDragDropPayload("object");
+            if (payload && payload->IsDelivery())
+            {
+                if (FrameGraph::Graph* fg = dynamic_cast<FrameGraph::Graph*>(*(Object**)payload->Data))
+                {
+                    target->SetFrameGraph(fg);
+                }
+            }
+            ImGui::EndDragDropTarget();
+        }
+
+        ImGui::SameLine();
+        if (ImGui::Button("Clear"))
+        {
+            target->SetFrameGraph(nullptr);
+        }
+    }
+
+private:
+    static const char _register;
+};
+
+const char CameraInspector::_register = InspectorRegistry::Register<CameraInspector, Camera>();
+
+} // namespace Editor
---
+++ Source/Editor/Inspectors/Components/MeshRendererInspector.cpp
@@ -0,0 +1,84 @@
+#include "../../EditorState.hpp"
+#include "../Inspector.hpp"
+#include "Core/Component/MeshRenderer.hpp"
+
+namespace Editor
+{
+class MeshRendererInspector : public Inspector<MeshRenderer>
+{
+public:
+    void DrawInspector(GameEditor& editor) override
+    {
+        MeshRenderer* meshRenderer = target;
+
+        Mesh* mesh = meshRenderer->GetMesh();
+
+        std::string meshGUIID = "emtpy";
+        if (mesh)
+            meshGUIID = mesh->GetName();
+
+        ImGui::Text("Mesh: ");
+        ImGui::SameLine();
+
+        bool bp = ImGui::Button(meshGUIID.c_str());
+        if (ImGui::BeginDragDropTarget())
+        {
+            const ImGuiPayload* payload = ImGui::AcceptDragDropPayload("object");
+            if (payload && payload->IsDelivery())
+            {
+                Object& obj = **(Object**)payload->Data;
+                if (typeid(obj) == typeid(Mesh))
+                {
+                    Mesh* mesh = static_cast<Mesh*>(&obj);
+
+                    meshRenderer->SetMesh(mesh);
+                    mesh = meshRenderer->GetMesh();
+                }
+            }
+            ImGui::EndDragDropTarget();
+        }
+        else if (bp)
+        {
+            EditorState::selectedObject = mesh;
+        }
+        // show materials
+        ImGui::Text("Materials: ");
+        std::vector<Material*> mats = meshRenderer->GetMaterials();
+        for (int i = 0; i < mats.size(); ++i)
+        {
+            Material* mat = mats[i];
+            std::string buttonID;
+            if (mat)
+                buttonID = fmt::format("{}: {}", i, mats[i]->GetName());
+            else
+                buttonID = fmt::format("{}##{}", "emtpy", i);
+
+            if (ImGui::Button(buttonID.c_str()))
+            {
+                EditorState::selectedObject = mats[i];
+            };
+
+            if (ImGui::BeginDragDropTarget())
+            {
+                const ImGuiPayload* payload = ImGui::AcceptDragDropPayload("object");
+                if (payload && payload->IsDelivery())
+                {
+                    Object& obj = **(Object**)payload->Data;
+                    if (typeid(obj) == typeid(Material))
+                    {
+                        mats[i] = static_cast<Material*>(&obj);
+                        meshRenderer->SetMaterials(mats);
+                    }
+                }
+                ImGui::EndDragDropTarget();
+            }
+        }
+    }
+
+private:
+    static const char _register;
+};
+
+const char MeshRendererInspector::_register = InspectorRegistry::Register<MeshRendererInspector, MeshRenderer>();
+
+} // namespace Editor
