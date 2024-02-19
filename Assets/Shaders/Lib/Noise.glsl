74f4ce46dc81ce47f64bd3b9dcd6805f0a4ddfe3
Jiehong Jiang <jjiangweilan@gmail.com>
2024-02-19 19:03:46 +0800

    add worley noise

--- Assets/Shaders/Lib/Noise.glsl
+++ Assets/Shaders/Lib/Noise.glsl
@@ -43,4 +43,42 @@ float perlinNoise3D(vec3 pos)
 }
 #endif
 
+// worley noise
+// from https://www.shadertoy.com/view/MstGRl
+#if defined(USE_WORLEY_NOISE) && defined(NOISE_WORLEY_NOISE_NUM_CELLS)
+
+// Arbitrary random, can be replaced with a function of your choice
+float rand(vec2 co){
+    return fract(sin(dot(co.xy ,vec2(12.9898,78.233))) * 43758.5453);
+}
+
+// Returns the point in a given cell
+vec2 get_cell_point(ivec2 cell) {
+	vec2 cell_base = vec2(cell) / NOISE_WORLEY_NOISE_NUM_CELLS;
+	float noise_x = rand(vec2(cell));
+    float noise_y = rand(vec2(cell.yx));
+    return cell_base + (0.5 + 1.5 * vec2(noise_x, noise_y)) / NOISE_WORLEY_NOISE_NUM_CELLS;
+}
+
+// Performs worley noise by checking all adjacent cells
+// and comparing the distance to their points
+float worley(vec2 coord) {
+    ivec2 cell = ivec2(coord * NOISE_WORLEY_NOISE_NUM_CELLS);
+    float dist = 1.0;
+    
+    // Search in the surrounding 5x5 cell block
+    for (int x = 0; x < 5; x++) { 
+        for (int y = 0; y < 5; y++) {
+        	vec2 cell_point = get_cell_point(cell + ivec2(x-2, y-2));
+            dist = min(dist, distance(cell_point, coord));
+
+        }
+    }
+    
+    dist /= length(vec2(1.0 / NOISE_WORLEY_NOISE_NUM_CELLS));
+    dist = 1.0 - dist;
+    return dist;
+}
+#endif
+
 #endif
