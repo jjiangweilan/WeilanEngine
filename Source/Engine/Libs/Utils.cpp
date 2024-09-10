cd4d6e3676ec62d93a0364e306ded9bdec796eb9
Jiehong Jiang <jiehong.jiang@lilith.com>
2024-09-10 13:16:33 +0800

    feat: multi extension supports

--- Source/Engine/Libs/Utils.cpp
+++ Source/Engine/Libs/Utils.cpp
@@ -1,6 +1,7 @@
 #include "Utils.hpp"
 #include <algorithm>
 #include <cctype>
+#include <sstream>
 namespace Utils
 {
 std::string strTolower(const std::string& ss)
@@ -21,5 +22,17 @@ uint32_t GetPadding(uint32_t address, uint32_t alignment)
         return 0;
 
     return (alignment - (address & (alignment - 1))) & (alignment - 1);
+}
+
+std::vector<std::string> SplitString(const std::string& s, char delimiter)
+{
+    std::stringstream ss(s);
+    std::vector<std::string> tokens;
+    std::string token;
+    while (getline(ss, token, delimiter))
+    {
+        tokens.push_back(token);
+    }
+    return tokens;
 }
 } // namespace Utils
