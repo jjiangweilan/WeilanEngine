10cca33c6052e8b5ae595f8863c413711dd344fc
Jiehong Jiang <jjiangweilan@gmail.com>
2024-12-30 22:22:24 +0800

    add screen space shadow shader

--- Source/Engine/Rendering/ShaderLibrary.hpp
+++ Source/Engine/Rendering/ShaderLibrary.hpp
@@ -71,6 +71,7 @@ public:
     static constexpr const char* PostProcess_OutlineRawColorPass = "PostProcess/Outline/OutlineRawColorPass";
     static constexpr const char* PostProcess_SSAO = "PostProcess/SSAO";
     static constexpr const char* ShadowMapObject = "ShadowMapObject";
+    static constexpr const char* ScreenSpaceShadow = "ScreenSpaceShadow";
 
     static ObjPtr<Shader2> GetShader(const char* name, ShaderPermutation permutation = ShaderPermutation())
     {
