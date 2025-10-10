c73298b955b17a4c023faca3139254bc0ecc292b
Jiehong Jiang <jjiangweilan@gmail.com>
2025-10-10 19:12:34 +0800

    update

--- Source/Engine/Rendering/ShaderLibrary.hpp
+++ Source/Engine/Rendering/ShaderLibrary.hpp
@@ -9,38 +9,34 @@
 #include <spdlog/spdlog.h>
 #include <unordered_map>
 
+#define SHADER_ENUMS(Do) Do(DeferredPBRShading, "DeferredPBRShading"), Do(SceneLit, "SceneLit"),             \
+                         Do(SceneLitSkinned, "SceneLitSkinned"), Do(PlaneGrid, "PlaneGrid"),                 \
+                         Do(ImGui, "ImGui"), Do(LineShader, "LineShader"),                                   \
+                         Do(TriangleShader, "TriangleShader"),                                               \
+                         Do(JoltDebugShader, "Specific/JoltDebugShader"),                                    \
+                         Do(PostProcess_OutlineFullScreenPass, "PostProcess/Outline/OutlineFullScreenPass"), \
+                         Do(PostProcess_OutlineRawColorPass, "PostProcess/Outline/OutlineRawColorPass"),     \
+                         Do(PostProcess_SSAO, "PostProcess/SSAO"),                                           \
+                         Do(ShadowMapObject, "ShadowMapObject"),                                             \
+                         Do(ShadowMapObjectSkinned, "ShadowMapObjectSkinned"),                               \
+                         Do(ScreenSpaceShadow, "ScreenSpaceShadow"), Do(FXAA, "FXAA"),                       \
+                         Do(PrimitiveShape, "PrimitiveShape"),                                               \
+                         Do(SimpleForwardLit, "SimpleForwardLit"),                                           \
+                         Do(SimpleColor, "SimpleColor"), Do(Skybox, "Skybox"),                               \
+                         Do(ColorGrading, "ColorGrading"),                                                   \
+                         Do(InterleavedGradientNoise, "InterleavedGradientNoise"),                           \
+                         Do(Particle, "Particles/Particle"), Do(Blit, "Blit"),                               \
+                         Do(BilateralUpScale, "BilateralUpScale"),                                           \
+                         Do(DepthDownSampler, "DepthDownSampler"),                                           \
+                         Do(ContactShadow, "ContactShadow/ContactShadow"),                                   \
+                         Do(Ocean, "Ocean"),
+
+#define _SHADER_ENUMS_PICK_FIRST(x, y) x
+#define _SHADER_ENUMS_PICK_SECOND(x, y) y
+
 enum class Shaders : int
 {
-    DeferredPBRShading,
-    SceneLit,
-    SceneLitSkinned,
-    PlaneGrid,
-    ImGui,
-    LineShader,
-    TriangleShader,
-    JoltDebugShader,
-    PostProcess_OutlineFullScreenPass,
-    PostProcess_OutlineRawColorPass,
-    PostProcess_SSAO,
-    ShadowMapObject,
-    ShadowMapObjectSkinned,
-    ScreenSpaceShadow,
-    FXAA,
-    PrimitiveShape,
-    SimpleForwardLit,
-    SimpleColor,
-    // VolumetricCloud,
-    Skybox,
-    ColorGrading,
-    InterleavedGradientNoise,
-    // SHProbe,
-    Particle,
-    Blit,
-    BilateralUpScale,
-    DepthDownSampler,
-    ContactShadow,
-    Ocean,
-    MAX_COUNT
+    SHADER_ENUMS(_SHADER_ENUMS_PICK_FIRST) MAX_COUNT
 };
 
 class ShaderLibrary
@@ -49,8 +45,8 @@ class ShaderLibrary
     {
         CompiledShader() : shader(nullptr), permutation() {}
         CompiledShader(std::unique_ptr<Gfx::ShaderProgram>&& shader, ShaderPermutation permutation)
-            : shader(std::move(shader)), shaderHandle(this->shader.get()), permutation(permutation)
-        {}
+            : shader(std::move(shader)), shaderHandle(this->shader.get()),
+              permutation(permutation) {}
         CompiledShader(CompiledShader&& other) = default;
 
         std::unique_ptr<Gfx::ShaderProgram> shader;
@@ -74,35 +70,7 @@ class ShaderLibrary
 
 public:
     static constexpr const char* ShaderNameMap[] = {
-        "DeferredPBRShading",
-        "SceneLit",
-        "SceneLitSkinned",
-        "PlaneGrid",
-        "ImGui",
-        "LineShader",
-        "TriangleShader",
-        "Specific/JoltDebugShader",
-        "PostProcess/Outline/OutlineFullScreenPass",
-        "PostProcess/Outline/OutlineRawColorPass",
-        "PostProcess/SSAO",
-        "ShadowMapObject",
-        "ShadowMapObjectSkinned",
-        "ScreenSpaceShadow",
-        "FXAA",
-        "PrimitiveShape",
-        "SimpleForwardLit",
-        "SimpleColor",
-        // "VolumetricCloud",
-        "Skybox",
-        "ColorGrading",
-        "InterleavedGradientNoise",
-        // "SHProbe",
-        "Particles/Particle",
-        "Blit",
-        "BilateralUpScale",
-        "DepthDownSampler",
-        "ContactShadow/ContactShadow",
-        "Ocean",
+        SHADER_ENUMS(_SHADER_ENUMS_PICK_SECOND)
     };
 
     // ***** Deprecating *****
@@ -114,11 +82,14 @@ public:
     static constexpr const char* LineShader = "LineShader";
     static constexpr const char* TriangleShader = "TriangleShader";
     static constexpr const char* JoltDebugShader = "Specific/JoltDebugShader";
-    static constexpr const char* PostProcess_OutlineFullScreenPass = "PostProcess/Outline/OutlineFullScreenPass";
-    static constexpr const char* PostProcess_OutlineRawColorPass = "PostProcess/Outline/OutlineRawColorPass";
+    static constexpr const char* PostProcess_OutlineFullScreenPass =
+        "PostProcess/Outline/OutlineFullScreenPass";
+    static constexpr const char* PostProcess_OutlineRawColorPass =
+        "PostProcess/Outline/OutlineRawColorPass";
     static constexpr const char* PostProcess_SSAO = "PostProcess/SSAO";
     static constexpr const char* ShadowMapObject = "ShadowMapObject";
-    static constexpr const char* ShadowMapObjectSkinned = "ShadowMapObjectSkinned";
+    static constexpr const char* ShadowMapObjectSkinned =
+        "ShadowMapObjectSkinned";
     static constexpr const char* ScreenSpaceShadow = "ScreenSpaceShadow";
     static constexpr const char* FXAA = "FXAA";
     static constexpr const char* PrimitiveShape = "PrimitiveShape";
@@ -127,29 +98,42 @@ public:
     static constexpr const char* VolumetricCloud = "VolumetricCloud";
     static constexpr const char* Skybox = "Skybox";
     static constexpr const char* ColorGrading = "ColorGrading";
-    static constexpr const char* InterleavedGradientNoise = "InterleavedGradientNoise";
+    static constexpr const char* InterleavedGradientNoise =
+        "InterleavedGradientNoise";
     static constexpr const char* SHProbe = "SHProbe";
     static constexpr const char* Particle = "Particles/Particle";
     // **************
     //
 
-    static const char* GetShaderName(Shaders shader) { return ShaderNameMap[(int)shader]; }
+    static const char* GetShaderName(Shaders shader)
+    {
+        return ShaderNameMap[(int)shader];
+    }
 
-    static ObjPtr<Shader2> GetShader(Shaders shader, ShaderPermutation permutation = ShaderPermutation())
+    static ObjPtr<Shader2>
+    GetShader(Shaders shader, ShaderPermutation permutation = ShaderPermutation())
     {
         return Singleton().GetShaderImpl(ShaderNameMap[(int)shader], permutation);
     }
 
-    static ObjPtr<Shader2> GetShader(const char* name, ShaderPermutation permutation = ShaderPermutation())
+    static ObjPtr<Shader2>
+    GetShader(const char* name, ShaderPermutation permutation = ShaderPermutation())
     {
         return Singleton().GetShaderImpl(name, permutation);
     }
 
-    static void CompileAllDefaultShaders() { return Singleton().CompileAllDefaultShadersImpl(); }
-
-    static ObjPtr<Shader2> GetShader(const char* name, const std::vector<std::string>& permutations)
+    static void CompileAllDefaultShaders()
     {
-        return Singleton().GetShaderImpl(name, QueryShaderFeatures(name).GetPermutation(permutations));
+        return Singleton().CompileAllDefaultShadersImpl();
+    }
+
+    static ObjPtr<Shader2>
+    GetShader(const char* name, const std::vector<std::string>& permutations)
+    {
+        return Singleton().GetShaderImpl(
+            name,
+            QueryShaderFeatures(name).GetPermutation(permutations)
+        );
     }
 
     static const ShaderFeatures& QueryShaderFeatures(const char* name)
@@ -159,7 +143,10 @@ public:
 
     static void WaitForShaderCompilation() { Singleton().WaitForAllImpl(); }
 
-    static void DestorySlangInstance() { return Singleton().DestorySlangInstanceImpl(); }
+    static void DestorySlangInstance()
+    {
+        return Singleton().DestorySlangInstanceImpl();
+    }
 
     static void ReloadAllShaders() { return Singleton().ReloadAllShadersImpl(); }
 
@@ -180,11 +167,16 @@ private:
     void LoadSession();
     void WaitForAllImpl() { asyncWorker.WaitForAll(); }
     void DestorySlangInstanceImpl();
-    ObjPtr<Shader2> GetShaderImpl(const char* name, ShaderPermutation permutation = ShaderPermutation());
+    ObjPtr<Shader2>
+    GetShaderImpl(const char* name, ShaderPermutation permutation = ShaderPermutation());
     const ShaderFeatures& QueryShaderFeaturesImpl(const char* name);
     void ReloadAllShadersImpl();
-    inline const char* GetShaderRootPath() { return ENGINE_SOURCE_PATH "/Source/Engine/Shaders/"; }
-    std::unique_ptr<Gfx::ShaderProgram> CompileShader(const char* shaderName, ShaderPermutation permutation);
+    inline const char* GetShaderRootPath()
+    {
+        return ENGINE_SOURCE_PATH "/Source/Engine/Shaders/";
+    }
+    std::unique_ptr<Gfx::ShaderProgram>
+    CompileShader(const char* shaderName, ShaderPermutation permutation);
     const ShaderFeatures& RetriveShaderFeatures(const char* shaderName);
     void CollectToggleFeatures(slang::IModule* module, std::vector<ShaderToggleFeature>& outFeatures);
     void CompileAllDefaultShadersImpl();
