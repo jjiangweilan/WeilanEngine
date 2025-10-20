410844984d77658c5cc3ca0267542b5eadecb7e5
Jiehong Jiang <jjiangweilan@gmail.com>
2025-10-20 10:13:33 +0800

    update

--- Source/Engine/Rendering/CommandBufferUitls.cpp
+++ Source/Engine/Rendering/CommandBufferUitls.cpp
@@ -4,6 +4,11 @@ namespace Rendering
 {
 void DrawMesh(Gfx::CommandBuffer& cmd, Mesh& mesh, Material& material, const float4x4& transform, int materialSet)
 {
+    struct T
+    {
+        float4x4 m;
+        float4x4 inv;
+    };
     auto materialResource = material.GetShaderResource();
     auto shaderProgram = material.GetShaderProgram();
 
@@ -12,12 +17,13 @@ void DrawMesh(Gfx::CommandBuffer& cmd, Mesh& mesh, Material& material, const flo
         auto shader = material.GetShader();
         if (shader != nullptr)
         {
+            T mm{transform, glm::inverse(transform)};
             cmd.BindVertexBuffer(submesh.GetGfxVertexBufferBindings(), 0);
             cmd.BindIndexBuffer(submesh.GetIndexBuffer(), 0, submesh.GetIndexBufferType());
             if (materialSet >= 0)
                 cmd.BindResource(materialSet, materialResource);
             cmd.BindShaderProgram(shaderProgram, material.GetShaderConfig());
-            cmd.SetPushConstant(shaderProgram, (void*)&transform);
+            cmd.SetPushConstant(shaderProgram, (void*)&mm);
             cmd.DrawIndexed(submesh.GetIndexCount(), 1, 0, 0, 0);
         }
     }
